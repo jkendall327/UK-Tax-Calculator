ca4493e579064057ef33ade805f74719a14f8e54
Jack Kendall <jkendall3096@gmail.com>
2021-06-07 12:24:01 +0100

    Add transaction table to DB

--- TaxCrud/DbHelper.cs
+++ TaxCrud/DbHelper.cs
@@ -15,34 +15,44 @@ namespace TaxCrud
 
         internal void Initialize()
         {
-            Connection?.Execute("CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR(20), LastName VARCHAR(20));");
+            Connection.Execute("CREATE TABLE IF NOT EXISTS Users " +
+                "(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "FirstName VARCHAR(20), " +
+                "LastName VARCHAR(20));");
+
+            Connection.Execute("CREATE TABLE IF NOT EXISTS Transactions " +
+                "(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "Amount INTEGER, " +
+                "Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                "transuser INTEGER, " +
+                "FOREIGN KEY(transuser) REFERENCES Users(Id));");
         }
 
         internal const string GetUserDetails = "SELECT [Id], [FirstName],[LastName] FROM [Users]";
 
         internal void AddUser(string firstName, string lastName)
         {
-            Connection?.Execute("INSERT INTO Users (FirstName, LastName) VALUES (@fname,@lname)", new { fname = firstName, lname = lastName });
+            Connection.Execute("INSERT INTO Users (FirstName, LastName) VALUES (@fname,@lname)", new { fname = firstName, lname = lastName });
         }
 
         internal IEnumerable<Person> GetAllUsers()
         {
-            return Connection?.Query<Person>(GetUserDetails);
+            return Connection.Query<Person>(GetUserDetails);
         }
 
         internal IEnumerable<Person> GetByName(string firstName, string lastName)
         {
-            return Connection?.Query<Person>($"{GetUserDetails} WHERE FirstName = @fname AND LastName = @lname", new { fname = firstName, lname = lastName });
+            return Connection.Query<Person>($"{GetUserDetails} WHERE FirstName = @fname AND LastName = @lname", new { fname = firstName, lname = lastName });
         }
 
         internal void DeleteUser(int id)
         {
-            Connection?.Execute("DELETE FROM Users WHERE Id = @uid", new { uid = id });
+            Connection.Execute("DELETE FROM Users WHERE Id = @uid", new { uid = id });
         }
 
         internal void ResetDatabase()
         {
-            Connection?.Execute("DROP TABLE [Users];");
+            Connection.Execute("DROP TABLE [Users];");
             Initialize();
         }
 
@@ -53,7 +63,7 @@ namespace TaxCrud
 
         internal void UpdateName(int id, string firstName, string lastName)
         {
-            Connection?.Execute($"UPDATE [Users] SET [FirstName] = '{firstName}', [LastName] = '{lastName}' WHERE [Id] = {id};");
+            Connection.Execute($"UPDATE [Users] SET [FirstName] = '{firstName}', [LastName] = '{lastName}' WHERE [Id] = {id};");
         }
     }
 }
